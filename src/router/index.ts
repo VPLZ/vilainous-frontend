d9f4ac3dfaa72c1bb05c117a08a686376a06df83
Plazma <vdzlucas@gmail.com>
2025-03-22 23:14:55 +0100

    Token Fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,22 +35,21 @@ router.beforeEach(async (to) => {
       token = value.split('=').pop()
     }
   })
-  console.log(token)
-  if (!token && to.name !== 'login') return { name: 'login' }
+  if (!token && to.name !== 'login' && to.name !== 'register') return { name: 'login' }
   try {
-    await axios
-      .get(globalVariables.API_URL + '/authenticate', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        if (response.status != 200 && to.name !== 'login') {
-          return { name: 'login' }
-        }
-      })
+    const response = await axios.get(globalVariables.API_URL + '/authenticate', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    if (response.status !== 200) {
+      console.warn('Token invalide, redirection vers login.')
+      return { name: 'login' }
+    }
   } catch (error) {
-    console.log(error)
+    console.log("Erreur d'authentification:", error)
+    document.cookie = 'authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+    if (to.name !== 'login' && to.name !== 'register') return { name: 'login' }
   }
 })
 export default router
