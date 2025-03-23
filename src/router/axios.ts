399998847438ce6ea8f74b43d5dca94d65d275cf
VPLZ <151134367+VPLZ@users.noreply.github.com>
2025-03-23 13:11:45 +0100

    Logout feature (#10)
    * Logout feature
    
    * Fix logout problems
    
    * Remove useless log

---
+++ src/router/axios.ts
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import router from '.'
+import { globalVariables } from '@/stores/global_variables'
+
+const api = axios.create({
+  baseURL: globalVariables.API_URL,
+})
+
+// Ajouter le token avant chaque requête
+api.interceptors.request.use(
+  (config) => {
+    const cookies = document.cookie
+    const values = cookies.split(';')
+    let token
+    values.forEach((value) => {
+      if (value.includes('authToken')) {
+        token = value.split('=').pop()
+      }
+    })
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+// Gérer les erreurs 401 et rafraîchir le token
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !document.URL.includes('/login') &&
+      !document.URL.includes('/register')
+    ) {
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default api
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,40 +3,34 @@
     <h1>Welcome to the Home Page</h1>
     <p>Here you can see all the posts</p>
     <button @click="apiTest">Test API</button>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script lang="ts">
 import { globalVariables } from '@/stores/global_variables'
-
-function apiTest(event: Event) {
+import api from '../router/axios'
+async function logout(event: Event) {
   event.preventDefault()
-  fetch(globalVariables.API_URL + '/', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(async (response) => {
-      if (response.status === 200) {
-        const jsonResponse = await response.json()
-        console.log(jsonResponse)
-      }
-      if (response.status.toString().startsWith('4')) {
-        console.log('Error in request')
-      }
-      if (response.status.toString().startsWith('5')) {
-        console.log('Internal Server Error')
-      }
-    })
-    .catch((error) => {
-      console.error('Problem with api' + error)
-    })
+  const response = await api.post(globalVariables.API_URL + '/logout')
+  if (response.status == 200) {
+    console.log(response)
+    document.cookie = 'authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+    window.location.reload()
+  }
+}
+async function apiTest(event: Event) {
+  event.preventDefault()
+  const response = await api.get(globalVariables.API_URL + '/')
+  if (response.status == 200) {
+    console.log(response.data)
+  }
 }
 
 export default {
   methods: {
     apiTest,
+    logout,
   },
 }
 </script>
